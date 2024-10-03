98a4ecf4868672f55ca5da47afd7552ed6782d53
Fer <feroxtv@gmail.com>
2024-10-03 13:02:38 +0200

    InitialCommit
    InitialCommit

---
+++ src/test/java/com/fer/Practia1JPA/Practica1JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.Practia1JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
