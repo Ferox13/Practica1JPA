fde7a240eaec61776c557649ca1109d2c7d65941
Fer <feroxtv@gmail.com>
2024-10-08 23:42:38 +0200

    Recursividad
    Arreglado recursividad infinita

--- src/main/java/com/fer/practica1jpa/model/dto/RegistroDTO.java
+++ src/main/java/com/fer/practica1jpa/model/dto/RegistroDTO.java
@@ -5,23 +5,15 @@ import java.time.LocalDate;
 public class RegistroDTO {
 
     private Long id;
-    private ClienteDTO cliente;
-    private LocalDTO local;
     private LocalDate fechaRegistro;
 
     public RegistroDTO() {
     }
 
-  
-
-    public RegistroDTO(Long id, ClienteDTO cliente, LocalDTO local, LocalDate fechaRegistro) {
+    public RegistroDTO(Long id, LocalDate fechaRegistro) {
         this.id = id;
-        this.cliente = cliente;
-        this.local = local;
         this.fechaRegistro = fechaRegistro;
     }
-
-
 
     public Long getId() {
         return id;
@@ -30,9 +22,6 @@ public class RegistroDTO {
     public void setId(Long id) {
         this.id = id;
     }
-
-   
-
 
     public LocalDate getFechaRegistro() {
         return fechaRegistro;
@@ -45,30 +34,6 @@ public class RegistroDTO {
     @Override
     public String toString() {
         return "RegistroDTO [id=" + id + ", fechaRegistro=" + fechaRegistro + "]";
-    }
-
-
-
-    public LocalDTO getLocal() {
-        return local;
-    }
-
-
-
-    public void setLocal(LocalDTO local) {
-        this.local = local;
-    }
-
-
-
-    public ClienteDTO getCliente() {
-        return cliente;
-    }
-
-
-
-    public void setCliente(ClienteDTO cliente) {
-        this.cliente = cliente;
     }
 
 }
